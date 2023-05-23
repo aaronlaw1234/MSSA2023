87796e6346c191c96794f0a32f238e5612dec6a7
Aaron Ackerlaw <aaronlaw1234@outlook.com>
2023-05-22 23:50:42 -0700

    random number code

---
+++ random number.ps1
@@ -0,0 +1,8 @@
+﻿
+$numbertoguess = 1..100| get-random
+do {
+  [int]$UserGuess = Read-Host -Prompt "Please enter a number between 1 and 100"
+  if ($UserGuess -lt $NumberToGuess) {Write-Host "Pick a higher number"}
+  elseif ($UserGuess -gt $NumberToGuess) {Write-Host "Pick a lower number"}
+  else {Write-Host "Congrats! you picked the correct number"}
+} until ($NumberToGuess -eq $UserGuess)
